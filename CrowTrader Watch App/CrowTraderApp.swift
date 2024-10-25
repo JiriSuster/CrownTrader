7686a953e149e0b97b3e6236009f90aac5ed4bbf
Jiří Daniel Šuster <xsuster@node.mendelu.cz>
2024-10-25 20:16:28 +0200

    Basic DI for watch

--- CrowTrader Watch App/CrowTraderApp.swift
+++ CrowTrader Watch App/CrowTraderApp.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 @main
+@MainActor
 struct CrowTrader_Watch_AppApp: App {
+    private let container = DIContainer()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
