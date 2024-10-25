6aff2354387a15decba932db430028558ee233ad
Jiří Daniel Šuster <xsuster@node.mendelu.cz>
2024-10-25 18:02:46 +0200

    initial commit

---
+++ vi2_project Watch App/vi2_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  vi2_projectApp.swift
+//  vi2_project Watch App
+//
+//  Created by Jiří Daniel Šuster on 25.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct vi2_project_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
